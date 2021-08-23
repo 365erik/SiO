8e2d8df54106ba36b1243de60dae652126df3ee3
Erik Smith <github@codeblind.io>
2021-08-22 22:40:19 -0400

    Add ToTop (#7)
    Adds a button to jump to top.

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 import menu from "./menu";
 import tagline from "./tagline";
+import topButton from "./topButton";
 
 document.addEventListener("DOMContentLoaded", () => {
   menu({toggleID: "MainNavigationToggle", menuID: "MainNavigationMenu", openClass: "is-open"});
   tagline({taglineID: "Tagline"});
+  topButton();
 });
