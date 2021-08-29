caa6374ca99fc3f65fc051d328074c77ef5cdf78
Erik Smith <github@codeblind.io>
2021-08-29 16:45:09 -0400

    A lot of work on styles and menus

--- src/js/topButton.js
+++ src/js/topButton.js
@@ -1,43 +1,42 @@
 const tobButton = () => {
-		const button = document.getElementById("ToTop");
-		if (button) {
-				button.addEventListener('click', (e) => {
-						e.preventDefault();
-						window.scrollTo(0, 0);
-				});
-		} else {
-				return;
-		}
+  const button = document.getElementById("ToTop");
+  let lastKnownScrollPosition = 0;
+  let isShowing = false;
+  let ticking = false;
 
-		let lastKnownScrollPosition = 0;
-		let isShowing = false;
-		let ticking = false;
+  if (button) {
+    button.addEventListener("click", (e) => {
+      e.preventDefault();
+      window.scrollTo(0, 0);
+    });
+  } else {
+    return;
+  }
 
-		const toggleButton = () => {
-				if (lastKnownScrollPosition > window.innerHeight) {
-						if (!isShowing) {
-								button.classList.add("show");
-								isShowing = true;
-						}
-				} else if (isShowing) {
-						isShowing = false;
-						setTimeout(() => {
-								button.classList.remove("show")
-						}, 200);
-				}
-		};
+  const toggleButton = () => {
+    if (lastKnownScrollPosition > window.innerHeight) {
+      if (!isShowing) {
+        button.classList.remove("hide");
+        button.classList.add("show");
+        isShowing = true;
+      }
+    } else if (isShowing) {
+      button.classList.remove("show");
+      button.classList.add("hide");
+      isShowing = false;
+    }
+  };
 
-		document.addEventListener("scroll", (e) => {
-				lastKnownScrollPosition = window.scrollY;
-				if (!ticking) {
-						window.requestAnimationFrame(() => {
-								toggleButton();
-								ticking = false;
-						});
-						ticking = true;
-				}
-		});
-
-}
+  document.addEventListener("scroll", (e) => {
+    lastKnownScrollPosition = window.scrollY;
+    if (!ticking) {
+      window.requestAnimationFrame(() => {
+        toggleButton();
+        ticking = false;
+      });
+      ticking = true;
+    }
+  });
+};
 
 export default tobButton;
