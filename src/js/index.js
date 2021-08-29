a74c7329e95e92c44104dafc7633fa0d41230c34
Erik Smith <github@codeblind.io>
2021-08-29 16:49:10 -0400

    A lot of work on styles and menus (#9)

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,11 @@ import tagline from "./tagline";
 import topButton from "./topButton";
 
 document.addEventListener("DOMContentLoaded", () => {
-  menu({toggleID: "MainNavigationToggle", menuID: "MainNavigationMenu", openClass: "is-open"});
-  tagline({taglineID: "Tagline"});
+  menu({
+    toggleID: "MainNavigationToggle",
+    menuID: "MainNavigationMenu",
+    openClass: "is-open",
+  });
+  tagline({ taglineID: "Tagline" });
   topButton();
 });
