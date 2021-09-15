032bca71c4823be5ac278c27e54cea7cfde7484f
Erik Smith <github@codeblind.io>
2021-09-15 07:16:56 -0400

    Remove top button in favor of shortcut

--- src/js/topButton.js
+++ src/js/topButton.js
@@ -1,48 +1,6 @@
 import { SHORTCUTS } from "./shortcuts";
-import { matchSiOGlobal } from "./SiOGlobals";
 
-const tobButton = () => {
-  const button = document.getElementById("ToTop");
-  let lastKnownScrollPosition = 0;
-  let isShowing = false;
-  let ticking = false;
-  let showButtonWhenScrolledThisMuch = window.innerHeight * 0.7;
-
+const tobButton = () =>
   document.addEventListener(SHORTCUTS.PAGE_TOP, () => window.scrollTo(0, 0));
-
-  if (button) {
-    button.addEventListener("click", (e) => {
-      e.preventDefault();
-      window.scrollTo(0, 0);
-    });
-  } else {
-    return;
-  }
-
-  const toggleButton = () => {
-    if (lastKnownScrollPosition > showButtonWhenScrolledThisMuch) {
-      if (!isShowing && !matchSiOGlobal("modal", "open")) {
-        button.classList.remove("hide");
-        button.classList.add("show");
-        isShowing = true;
-      }
-    } else if (isShowing) {
-      button.classList.remove("show");
-      button.classList.add("hide");
-      isShowing = false;
-    }
-  };
-
-  document.addEventListener("scroll", (e) => {
-    lastKnownScrollPosition = window.scrollY;
-    if (!ticking) {
-      window.requestAnimationFrame(() => {
-        toggleButton();
-        ticking = false;
-      });
-      ticking = true;
-    }
-  });
-};
 
 export default tobButton;
